f421f7272a66cdd8dd19afb130030578e0acd8ca
Shylesh <shylesharepelly@gmail.com>
2023-03-06 16:59:44 +0530

    final

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function convertTime(timeString) {
 
 
 app.get("/", async (request, response)=> {
-  const slist=["10:00 AM" ,"10:30 AM", "11:00 AM" ,"11:30 AM" ,"12:00 PM ","12:30 PM", "1:00 PM ","1:30 PM","2:00 PM","2:30 PM","3:00 PM","3:30 PM","4:00 PM","4:30 PM",];
+  const slist=["10:00 AM" ,"10:30 AM", "11:00 AM" ,"11:30 AM" ,"12:00 PM ","12:30 PM", "1:00 PM ","1:30 PM","2:00 PM","2:30 PM","3:00 PM","3:30 PM","4:00 PM","4:30 PM"];
   const slist1=["10:00 AM" , "11:00 AM" ,"12:00 PM" ,"1:00 PM ","2:00 PM","3:00 PM","4:00 PM"];
   const totallist1 = await List.getlist();
   const list1 =  [];
